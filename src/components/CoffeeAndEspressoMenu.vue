3883291888a30a48be51c3cc4226c3f90590ca95
wons0421 <95213071+wons0421@users.noreply.github.com>
2022-11-30 11:05:17 +0900

    test

--- src/components/CoffeeAndEspressoMenu.vue
+++ src/components/CoffeeAndEspressoMenu.vue
@@ -1,9 +1,42 @@
 <template>
   <div>
-	  dd
+	  <input type="text" v-model="login.mberId">
+	  <input type="password" v-model="login.pw">
+	  <button @click="loginClick()">login</button>
+	  <br>
+	  <button @click="getDate()">getDate</button>
   </div>
 </template>
 
 <script setup>
+
+import {reactive, ref} from "vue";
+import axios from "axios";
+
+
+const login = reactive({
+	mberId: '',
+	pw: ''
+})
+
+let token = ref();
+
+const loginClick = async () => {
+	
+	const response = await axios.post('/api/login', login)
+	
+	token.value = response.data
+}
+
+const getDate = async () => {
+	const response = await axios.post('/api/curDate', null, {
+		headers: {
+			Authorization: 'Bearer ' + token.value
+		}
+	})
+	
+	console.log(response.data)
+}
+
 
 </script>
